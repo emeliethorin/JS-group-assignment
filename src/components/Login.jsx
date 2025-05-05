0cb0b353c5d6040b79fe34610cc255d3c69bb602
ShalishaManila <shalisha.manila@gmail.com>
2025-05-05 22:42:00 +0200

    created login and register pages

---
+++ src/components/Login.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const loggeduser = JSON.parse(localStorage.getItem("user"));
+    if (
+      input.email === loggeduser.email &&
+      input.password === loggeduser.password
+    ) {
+      localStorage.setItem("loggedin", true);
+      navigate("/");
+    } else {
+      alert("Wrong Email or Password");
+    }
+  };
+  return (
+    <>
+      <section
+        className="vh-100 bg-image"
+        // style={{ backgroundImage: "url('../assets/background.jpg')" }}
+      >
+        <div className="mask d-flex align-items-center h-100 ">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              {/* <div className="col-12 col-md-9 col-lg-7 col-xl-6"> */}
+              <div className="">
+                <div className="card" style={{ borderRadius: "15px" }}>
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5">Login</h2>
+                    <form onSubmit={handleLogin}>
+                      <div className="form-outline mb-4">
+                        <input
+                          name="email"
+                          value={input.email}
+                          onChange={(e) =>
+                            setInput({
+                              ...input,
+                              [e.target.name]: e.target.value,
+                            })
+                          }
+                          type="email"
+                          className="form-control form-control-lg"
+                          id="id_email"
+                          placeholder="Your Email"
+                        />
+                        {/* <label htmlFor="id_email" className="form-label">
+                          Your Email
+                        </label> */}
+                      </div>
+                      <div className="form-outline mb-4">
+                        <input
+                          name="password"
+                          value={input.password}
+                          onChange={(e) =>
+                            setInput({
+                              ...input,
+                              [e.target.name]: e.target.value,
+                            })
+                          }
+                          type="password"
+                          className="form-control form-control-lg"
+                          id="id_password"
+                          placeholder="password"
+                        />
+                        {/* <label htmlFor="id_password" className="form-label">
+                          Password
+                        </label> */}
+                      </div>
+                      <div className="d-flex justify-content-center">
+                        <button
+                          type="submit"
+                          className="btn btn-success btn-block btn-lg gradient-custom-4 text-body text-white"
+                        >
+                          Login
+                        </button>
+                      </div>
+                      <p className="text-center text-muted mt-5 mb-0">
+                        Don't have an account?
+                        <a href="/register" className="fw-bold text-body">
+                          <u>Register here</u>
+                        </a>
+                      </p>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
